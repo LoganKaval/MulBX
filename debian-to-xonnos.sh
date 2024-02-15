a9bd1f11a5d284037674141d5ad84e1feba8f82b
Logan Kaval <136924755+LoganKaval@users.noreply.github.com>
2024-02-15 17:05:15 -0600

    Screenshot Tool Added

--- debian-to-xonnos.sh
+++ debian-to-xonnos.sh
@@ -51,6 +51,8 @@ curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 apt update
 apt install brave-browser
+apt install kde-spectacle
+
 
 # Making Directories
 
