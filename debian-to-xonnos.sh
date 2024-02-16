d52ca9c26616e2259a3a4d6d20252082e99fef17
Logan Kaval <136924755+LoganKaval@users.noreply.github.com>
2024-02-16 13:50:52 -0600

    -y Flag Added

--- debian-to-xonnos.sh
+++ debian-to-xonnos.sh
@@ -22,36 +22,36 @@ sleep 10
 
 sudo apt update
 sudo apt upgrade
-sudo apt install tty-clock
-sudo apt install chromium-browser
-sudo apt install thunar
-sudo apt install libreoffice
-sudo apt install cheese
-sudo apt install git
-sudo apt install wget
-sudo apt install xmonad
-sudo apt install xmobar
-sudo apt install xterm
-sudo apt install terminator
-sudo apt install featherpad
-sudo apt install nitrogen
-sudo apt install compton
-sudo apt install vim
-sudo apt install neofetch
-sudo apt install ranger
-sudo apt install btop
-sudo apt install dmenu
-sudo apt install qt5ct
-sudo apt install gnome-clocks
-sudo apt install xfce4-power-manager
-sudo apt install nano
-sudo apt install mc
-sudo apt install curl
+sudo apt install tty-clock -y
+sudo apt install chromium-browser -y
+sudo apt install thunar -y
+sudo apt install libreoffice -y
+sudo apt install cheese -y
+sudo apt install git -y
+sudo apt install wget -y
+sudo apt install xmonad -y
+sudo apt install xmobar -y
+sudo apt install xterm -y
+sudo apt install terminator -y
+sudo apt install featherpad -y
+sudo apt install nitrogen -y
+sudo apt install compton -y
+sudo apt install vim -y
+sudo apt install neofetch -y
+sudo apt install ranger -y
+sudo apt install btop -y
+sudo apt install dmenu -y
+sudo apt install qt5ct -y
+sudo apt install gnome-clocks -y
+sudo apt install xfce4-power-manager -y
+sudo apt install nano -y
+sudo apt install mc -y
+sudo apt install curl -y
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
-sudo apt install brave-browser
-sudo apt install kde-spectacle
+sudo apt install brave-browser -y
+sudo apt install kde-spectacle -y
 
 
 # Making Directories
@@ -111,7 +111,7 @@ then
 	if [ "$kde" = "y" ];
 	then
 		sudo apt update
-		sudo apt install kde-plasma-desktop
+		sudo apt install kde-plasma-desktop -y
 
 	else
 		echo "KDE Will Not Be Installed!"
@@ -127,7 +127,7 @@ then
 	if [ "$gnome" = "y" ];
 	then
 		sudo apt update
-		sudo apt install task-gnome-desktop
+		sudo apt install task-gnome-desktop -y
 
 	else
 		echo "GNOME Will Not be Installed!"
@@ -142,7 +142,7 @@ then
 	if [ "$xfce" = "y" ];
 	then
 		sudo apt update
-		sudo apt install xfce4 xfce4-goodies
+		sudo apt install xfce4 xfce4-goodies -y
 
 	else
 		echo "Xfce Will Not be Installed!"
@@ -158,7 +158,7 @@ then
 	if [ "$lxqt" = "y" ];
 	then 
 		sudo apt update
-		sudo apt install lxqt-core lxqt task-lxqt-desktop
+		sudo apt install lxqt-core lxqt task-lxqt-desktop -y
 
 	else
 		echo "LXQT Will Not be Installed!"
@@ -174,7 +174,7 @@ then
 	if [ "$cin" = "y" ];
 	then
 		sudo apt update
-		sudo apt install task-cinnamon-desktop
+		sudo apt install task-cinnamon-desktop -y
 
 	else
 		echo "Cinnamon Will Not be Installed!"
@@ -190,7 +190,7 @@ then
 	if [ "$MATE" = "y" ];
 	then
 		sudo apt update
-		sudo apt install mate-desktop-environment mate-desktop-environment-extras
+		sudo apt install mate-desktop-environment mate-desktop-environment-extras -y
 	else
 		echo "MATE Will Not be Installed!"
 
@@ -204,7 +204,7 @@ then
 	if [ "$bud" = "y" ];
 	then
 		sudo apt update
-		sudo apt install budgie-desktop
+		sudo apt install budgie-desktop -y
 	else
 		echo "Budgie Will Not be Installed!"
 	fi
@@ -231,7 +231,7 @@ then
 	if [ "$nala" = "y" ];
 	then
 		sudo apt update
-		sudo apt install nala
+		sudo apt install nala -y
 	else
 		echo "Nala Will Not be Installed!"
 	fi
