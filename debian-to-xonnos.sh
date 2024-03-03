4aded0fe77ff410ac53e1f93b015a02f22717147
Logan Kaval <136924755+LoganKaval@users.noreply.github.com>
2024-03-02 18:44:04 -0600

    Less Bloat

--- debian-to-xonnos.sh
+++ debian-to-xonnos.sh
@@ -17,42 +17,7 @@ echo "The Root Account WILL NOT Work!!!"
 read dir
 
 sleep 10
-# Installing Software
 
-
-sudo apt update
-sudo apt upgrade
-sudo apt install tty-clock -y
-sudo apt install chromium-browser -y
-sudo apt install thunar -y
-sudo apt install libreoffice -y
-sudo apt install cheese -y
-sudo apt install git -y
-sudo apt install wget -y
-sudo apt install xmonad -y
-sudo apt install xmobar -y
-sudo apt install xterm -y
-sudo apt install terminator -y
-sudo apt install featherpad -y
-sudo apt install nitrogen -y
-sudo apt install compton -y
-sudo apt install vim -y
-sudo apt install neofetch -y
-sudo apt install ranger -y
-sudo apt install btop -y
-sudo apt install dmenu -y
-sudo apt install qt5ct -y
-sudo apt install gnome-clocks -y
-sudo apt install xfce4-power-manager -y
-sudo apt install nano -y
-sudo apt install mc -y
-sudo apt install curl -y
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install brave-browser -y
-sudo apt install kde-spectacle -y
-sudo apt install libghc-xmonad-contrib-dev
 
 # To Install Packages in the Package List
 chmod +x packages.sh
