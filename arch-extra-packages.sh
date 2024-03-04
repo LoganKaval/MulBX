65237f226be38c9dc61099365716aef75aa64350
Logan Kaval <136924755+LoganKaval@users.noreply.github.com>
2024-03-04 16:39:04 -0600

    Update arch-extra-packages.sh

--- arch-extra-packages.sh
+++ arch-extra-packages.sh
@@ -7,3 +7,5 @@ sudo pacman -S prusa-slicer -y
 sudo pacman -S gimp -y
 sudo pacman -S kdenlive -y
 sudo pacman -S libreoffice -y
+echo "[     1.41H_4.14u                ] Finished: 'arch-extra-packages.sh'  script--"
+echo "[     3.13A_1.41l_4.14l_1.9a_7.0h] Continuing: 'arch-to-xonnos.sh' script--"
