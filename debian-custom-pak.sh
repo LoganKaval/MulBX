6ae65337dcde76a284beddadcca11707eea34ec1
Logan Kaval <161742547+LoganKavalSchool@users.noreply.github.com>
2024-03-04 10:05:16 -0600

    Not Done

--- debian-custom-pak.sh
+++ debian-custom-pak.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+echo "What Package(s) Would You Like to Intall?"
+echo "Keep a Space Inbetween!"
+echo "Type Quit to Leave!"
+read paknm
+
+if [ "$paknm" = "quit" ];
+then
+    exit
+else
+    sudo pacman -Syu
+    sudo pacman -Syy
+    sudo pacman -S $paknm -y
