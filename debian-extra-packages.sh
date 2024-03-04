7e90b375d0f65df1acbb5156cfaf6257ec92cf99
Logan Kaval <161742547+LoganKavalSchool@users.noreply.github.com>
2024-03-04 09:46:02 -0600

    More Packages

---
+++ debian-extra-packages.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt autoremove -y
+sudo apt install freecad -y
+sudo apt install prusa-slicer -y
+sudo apt install gimp -y
+sudo apt install kdenlive -y
+sudo apt install libreoffice -y
