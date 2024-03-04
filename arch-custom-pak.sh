9df20fca288e88203a7d12362bb67dce217ea28d
Logan Kaval <136924755+LoganKaval@users.noreply.github.com>
2024-03-04 16:32:32 -0600

    Create arch-custom-pak.sh

---
+++ arch-custom-pak.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+
+echo "This Will Install All Packages that Are Listed!"
+echo "Keep a Space Inbetween!"
+echo "Type Quit to Leave!"
+sleep 7
+sudo pacman -Syu
+sudo pacman -Syy
+
+echo "What Package Do You Want To Install?"
+read paknm
+
+
+until [ "$paknm" = "quit" ]
+do
+	echo "What Package(s) Do You Want to Install?"
+	echo "You Can List Them With One Space Inbetween Each Package!"
+	read paknm
+
+	sudo pacman -S $paknm -y
+
+done
+
+echo "All Done"
+echo "[     3.13A_1.41l_4.14l_1.9a_7.0h] Continuing: 'arch-to-xonnos.sh' script--"
