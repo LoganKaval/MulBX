a43263ff4d6dfc336f33fb23867e3b7eb923778c
Logan Kaval <136924755+LoganKaval@users.noreply.github.com>
2024-03-04 16:38:19 -0600

    Update debian-extra-packages.sh

--- debian-extra-packages.sh
+++ debian-extra-packages.sh
@@ -8,3 +8,6 @@ sudo apt install prusa-slicer -y
 sudo apt install gimp -y
 sudo apt install kdenlive -y
 sudo apt install libreoffice -y
+
+echo "[     1.41H_4.14u                ] Finished: 'debian-extra-packages.sh'  script--"
+echo "[     3.13A_1.41l_4.14l_1.9a_7.0h] Continuing: 'debian-to-xonnos.sh' script--"
