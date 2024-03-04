95681d9dd7e97e9649000cba3f9679363371031f
Logan Kaval <161742547+LoganKavalSchool@users.noreply.github.com>
2024-03-04 09:44:14 -0600

    More Packages

--- arch-extra-packages.sh
+++ arch-extra-packages.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+sudo pacman -Syu
+sudo pacman -Syy
+sudo pacman -S freecad -y
+sudo pacman -S prusa-slicer -y
+sudo pacman -S gimp -y
+sudo pacman -S kdenlive -y
+sudo pacman -S libreoffice -y
