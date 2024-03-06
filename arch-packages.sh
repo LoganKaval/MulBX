9b0e26aaa88ae7522e89a1d51661ea5331d2ad63
Logan Kaval <136924755+LoganKaval@users.noreply.github.com>
2024-03-06 16:18:43 -0600

    Update arch-packages.sh

--- arch-packages.sh
+++ arch-packages.sh
@@ -35,4 +35,4 @@ sudo pacman -S spectacle -y
 sudo pacman -S ufw -y
 
 echo "[     1.41H_4.14u                ] Finished: 'arch-packages.sh'  script--"
-echo "[     3.13A_1.41l_4.14l_1.9a_7.0h] Continuing: 'arch-to-xonnos.sh' script--"
+echo "[     3.13A_1.41l_4.14l_1.9a_7.0h] Continuing: 'arch-to-mulbx.sh' script--"
