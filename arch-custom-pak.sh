4163b7387ef2486fc7225f42210368c864ff57d9
Logan Kaval <136924755+LoganKaval@users.noreply.github.com>
2024-03-06 16:17:04 -0600

    Update arch-custom-pak.sh

--- arch-custom-pak.sh
+++ arch-custom-pak.sh
@@ -25,4 +25,4 @@ done
 
 echo "All Done"
 echo "[     1.41H_4.14u                ] Finished: 'arch-custom-pak.sh'  script--"
-echo "[     3.13A_1.41l_4.14l_1.9a_7.0h] Continuing: 'arch-to-xonnos.sh' script--"
+echo "[     3.13A_1.41l_4.14l_1.9a_7.0h] Continuing: 'arch-to-mulbx.sh' script--"
