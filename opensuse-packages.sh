9572c8b4bcfdcdef520cc62914d5f711664b45d0
Logan Kaval <136924755+LoganKaval@users.noreply.github.com>
2024-04-11 16:13:19 -0500

    :new --file/v117

--- opensuse-packages.sh
+++ opensuse-packages.sh
@@ -3,38 +3,36 @@
 
 sudo zypper refresh
 sudo zypper update
-sudo zypper install tty-clock -y
-sudo apt install chromium-browser -y
-sudo apt install thunar -y
-sudo apt install libreoffice -y
-sudo apt install cheese -y
-sudo apt install git -y
-sudo apt install wget -y
-sudo apt install xmonad -y
-sudo apt install xmobar -y
-sudo apt install xterm -y
-sudo apt install terminator -y
-sudo apt install featherpad -y
-sudo apt install nitrogen -y
-sudo apt install slock -y
-sudo apt install compton -y
-sudo apt install vim -y
-sudo apt install neofetch -y
-sudo apt install ranger -y
-sudo apt install btop -y
-sudo apt install dmenu -y
-sudo apt install qt5ct -y
-sudo apt install gnome-clocks -y
-sudo apt install xfce4-power-manager -y
-sudo apt install nano -y
-sudo apt install mc -y
-sudo apt install curl -y
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install brave-browser -y
-sudo apt install kde-spectacle -y
-sudo apt install libghc-xmonad-contrib-dev -y
+sudo zypper install chromium -y
+sudo zypper install thunar -y
+sudo zypper install cheese -y
+sudo zypper install git -y
+sudo zypper install wget -y
+sudo zypper install xmonad -y
+sudo zypper install xmobar -y
+sudo zypper install xterm -y
+sudo zypper install terminator -y
+sudo zypper install featherpad -y
+sudo zypper install nitrogen -y
+sudo zypper install picom -y
+sudo zypper install vim -y
+sudo zypper install neofetch -y
+sudo zypper install ranger -y
+sudo zypper install htop -y
+sudo zypper install dmenu -y
+sudo zypper install qt5ct -y
+sudo zypper install lxappearance -y
+sudo zypper install gnome-clocks -y
+sudo zypper install xfce4-power-manager -y
+sudo zypper install nano - y
+sudo zypper install mc -y
+sudo zypper install curl -y
+sudo zypper refresh
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+sudo zypper addrepo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+sudo zypper install brave-browser
+sudo zypper install spectacle -y
+sudo zypper install ghc-xmonad-contrib-devel -y
 
 echo "[     1.41H_4.14u                ] Finished: 'opensuse-packages.sh'  script--"
 echo "[     3.13A_1.41l_4.14l_1.9a_7.0h] Continuing: 'opensuse-to-mulbx.sh' script--"
